0c774b033d2d28e28cf09a83141293a075da6fa3
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-09-17 09:16:56 -0700

    got rid of unneeded comment

--- lib.rs
+++ lib.rs
@@ -4,7 +4,6 @@
 extern crate reqwest;
 
 pub fn get_response(count: i32, urls: bool, https: bool) -> Result<Vec<String>, reqwest::Error> {
-    //let url = format!("http://shibe.online/api/shibes?count={}&urls={}&httpsUrls={}", count, urls, https);
     let resp = reqwest::blocking::get(format!("http://shibe.online/api/shibes?count={}&urls={}&httpsUrls={}", count, urls, https))?
         .json::<Vec<String>>()?;
     Ok(resp)
