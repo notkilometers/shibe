37247a2e79ab807be63bd7d750c1963371b72120
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-09-25 16:39:30 -0700

    added option for multiple animals

--- lib.rs
+++ lib.rs
@@ -3,8 +3,21 @@
 
 extern crate reqwest;
 
-pub fn get_response(count: i32, urls: bool, https: bool) -> Result<Vec<String>, reqwest::Error> {
-    let resp = reqwest::blocking::get(format!("http://shibe.online/api/shibes?count={}&urls={}&httpsUrls={}", count, urls, https))?
+pub enum Animal {
+    Shiba,
+    Cat,
+    Bird,
+}
+
+pub fn get_response(pic_type: Animal, count: i32, urls: bool, https: bool) -> Result<Vec<String>, reqwest::Error> {
+    let animal = match pic_type {
+        Animal::Shiba => { "shibes" },
+        Animal::Cat => { "cats" },
+        Animal::Bird => { "birds" },
+    };
+    
+    let resp = reqwest::blocking::get(format!("http://shibe.online/api/{}?count={}&urls={}&httpsUrls={}", animal, count, urls, https))?
         .json::<Vec<String>>()?;
+    
     Ok(resp)
 }
