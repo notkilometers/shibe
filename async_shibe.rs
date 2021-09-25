60cce986c7b39424dfbd7e5ddce9509be2b4042d
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-09-25 16:41:08 -0700

    added option for multiple animals

--- async_shibe.rs
+++ async_shibe.rs
@@ -2,10 +2,21 @@
 #![crate_name ="shibe"]
 
 extern crate reqwest;
-extern crate tokio;
 
-pub async fn get_response(count: i32, urls: bool, https: bool) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let resp = reqwest::get(format!("http://shibe.online/api/shibes?count={}&urls={}&httpsUrls={}", count, urls, https))
+pub enum Animal {
+    Shiba,
+    Cat,
+    Bird,
+}
+
+pub async fn get_response(pic_type: Animal, count: i32, urls: bool, https: bool) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let animal = match pic_type {
+        Animal::Shiba => { "shibes" },
+        Animal::Cat => { "cats" },
+        Animal::Bird => { "birds" },
+    };
+    
+    let resp = reqwest::get(format!("http://shibe.online/api/{}?count={}&urls={}&httpsUrls={}", animal, count, urls, https))
         .await?
         .json::<Vec<String>>()
         .await?;
